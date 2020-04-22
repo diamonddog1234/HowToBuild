ec09c3afd047199c107e67c89b07afdf2cf2bbb8
diamonddog1234 <diamonddog1234@gmail.com>
2020-04-22 11:48:14 +0700

    привет 3

---
+++ NodeJS/reactapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AuthWindow from "./components/AuthWindow"
+
+function App() {
+  return (
+    <div>
+      <AuthWindow></AuthWindow>
+    </div>
+  );
+}
+
+export default App;
