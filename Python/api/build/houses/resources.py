f47f4f86636438c2ade527e01c32b3b4f83c6e5c
diamonddog <diamonddog1234@gmail.com>
2020-05-27 17:23:30 +0700

    Привет 6

--- Python/api/build/houses/resources.py
+++ Python/api/build/houses/resources.py
@@ -8,6 +8,52 @@ from core.models import House, Street, District, HouseFilterView, PivotView
 
 from core.auth.jwt import check_role_validation, check_validation
 
+def add_house(json_data):
+    if 'street' not in json_data:
+        return jsonify({'msg': 'no_street'})
+    if 'district' not in json_data:
+        return jsonify({'msg': 'no_district'})
+    if 'minLayingDepth' not in json_data:
+        return jsonify({'msg': 'no_minLayingDepth'})
+    if 'maxLayingDepth' not in json_data:
+        return jsonify({'msg': 'maxLayingDepth'})
+    if 'number' not in json_data:
+        return jsonify({'msg': 'no_number'})
+
+    street = json_data['street']
+    district = json_data['district']
+    min_laying_depth = float(json_data['minLayingDepth'])
+    max_laying_depth = float(json_data['maxLayingDepth'])
+    number = str(json_data['number'])
+
+    db_street = get_database_session().query(Street).filter(Street.value == street).first()
+    if not db_street:
+        db_street = Street(value = street)
+        get_database_session().add(db_street)
+        get_database_session().flush()
+    db_district = get_database_session().query(District).filter(District.value == district).first()
+    if not db_district:
+        db_district = District(value = district)
+        get_database_session().add(db_district)
+        get_database_session().flush()
+
+    db_house = get_database_session().query(House).\
+        filter(House.street_id == db_street.id).\
+        filter(House.district_id == db_district.id).\
+        filter(House.number == number).\
+        first()
+    if not db_house:
+        house = House(street_id=db_street.id,
+                      district_id=db_district.id,
+                      min_laying_depth=min_laying_depth,
+                      max_laying_depth=max_laying_depth,
+                      number = number)
+        get_database_session().add(house)
+        get_database_session().commit()
+    else:
+        return jsonify({'msg': 'NOT_ORIGINAL_HOUSE'})
+    return jsonify({'msg': 'OK', 'house_id': house.id})
+
 
 class AddHouse(Resource):
     @check_role_validation(['ChangeRecord'])
